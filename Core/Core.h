5811e3ad0046cf3571c297d8852f18282026efc6
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-25 12:44:19 +0100

    Add Core framework

---
+++ Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Alexandre Freire García on 25/10/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
