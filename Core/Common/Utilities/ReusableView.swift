a110421df3e179152c1537999cfb6aaa43cf2162
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-25 19:36:14 +0100

    Add utilities

---
+++ Core/Common/Utilities/ReusableView.swift
@@ -0,0 +1,20 @@
+//
+//  ReusableView.swift
+//  Core
+//
+//  Created by Alexandre Freire García on 25/10/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
